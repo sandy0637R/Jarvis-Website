0f137a3fe7d5e5c68c5d6848766659f2a3b86964
sandy0637R <sandeshraut842@gmail.com>
2024-07-12 06:48:41 -0700

    Updated

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+let swiper = new Swiper('.card-content', {
+    // Optional parameters
+    loop:true,
+    grabCursor:true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
